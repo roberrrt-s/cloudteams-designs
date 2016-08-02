489214458e115e06efaa9c277d82bec4e246b3d2
Robert Spier <robert.spier@gmail.com>
2016-08-02 16:25:34 +0200

    Adjusted margin/padding (css) and added subpages in Modal

--- src/views/developer/dashboard/projects/project/team/member-new-popup.php
+++ src/views/developer/dashboard/projects/project/team/member-new-popup.php
@@ -6,7 +6,7 @@
 
 		<!-- Modal content-->
 		<div class="block block-fill">
-			<header class="article-header">
+			<header class="modal-header">
 				<div class="vertical-align">
 					<div class="middle">
 						<i class="icon icon-team"></i>
@@ -20,7 +20,14 @@
 
 			<div class="modal-body">
 				<div class="row">
-
+					<ul class="menu-subpage">
+						<li id="link-to-subpage-1" class="link-to-subpage active">Manually</li>
+						<li id="link-to-subpage-2" class="link-to-subpage">Address book</li>
+						<li id="link-to-subpage-3" class="link-to-subpage">Search</li>
+					</ul>
+					<section id="subpage-1" class="subpage active">Hello</section>
+					<section id="subpage-2" class="subpage">World</section>
+					<section id="subpage-3" class="subpage">!!!</section>			
 				</div>
 			</div>
 		</div>

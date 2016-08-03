fe36315ad65e0b77f254a5a89cc08f4777954d8e
Robert Spier <robert.spier@gmail.com>
2016-08-03 16:03:37 +0200

    finished team page, (no interaction yet)

--- src/views/developer/dashboard/projects/project/team/member-new-popup.php
+++ src/views/developer/dashboard/projects/project/team/member-new-popup.php
@@ -25,6 +25,7 @@
 						<li id="link-to-subpage-2" class="link-to-subpage">Address book</li>
 						<li id="link-to-subpage-3" class="link-to-subpage">Search</li>
 					</ul>
+					<hr>
 					<section id="subpage-1" class="subpage active">
 
 						<fieldset class="form-group required">
@@ -60,6 +61,8 @@
 								</select>
 							</div>
 						</fieldset>
+
+						<hr>
 
 						<fieldset class="form-group form-submit">
 							<a href="" data-dismiss="modal" class="btn-transparent" alt="cancel">Skip</a>
@@ -201,9 +204,99 @@
 								</div>
 							</section>
 						</div>
+						<div class="row">
+							<div class="vertical-align">
+								<div class="middle">
+									<a href="#nowhere"><h2 class="header-medium secondary load-more">Load more contacts</h2></a>
+								</div>
+							</div>
+						</div>
 					</section>
 
-					<section id="subpage-3" class="subpage">!!!</section>			
+					<section id="subpage-3" class="subpage">
+						<fieldset class="form-group form-search">
+							<div class="vertical-align">
+								<label for="new-member-name" class="control-label header-medium secondary">Search for a member</label>
+							</div>
+							<div class="vertical-align">
+								<div class="middle">
+									<div class="input-container search-member-container">
+										<input value="Jen" id="new-member-name" type="text" name="Name of new team member" placeholder="Type a name">
+									</div>								
+								</div>
+								<div class="middle">
+									<a href="" class="btn" alt="submit invite">Search</a>
+								</div>
+							</div>
+						</fieldset>
+						<hr>
+						<div class="row">
+							<section class="team-member-invite">
+								<div class="vertical-align">
+									<div class="middle">
+										<div class="img-circle" style='background-image: url("[[../../../../../../img/profile-picture-small.jpg]]")'></div>
+									</div>
+
+									<div class="middle member-name">
+										<h2 class="header-medium secondary">Helena van der Bergen</h2>
+									</div>
+
+									<div class="middle">
+										<div class="select-dropdown">
+											<i class="icon icon-arrow-down"></i>
+											<select name="campaign-start-year" id="campaign-start-year" onchange="" size="1">
+												<option value="" disabled="" selected="">Member role</option>
+												<option value="front">Front-end Developer</option>
+												<option value="back">Back-end Developer</option>
+												<option value="manager">Project Manager</option>
+												<option value="content">Content Manager</option>
+												<option value="interaction">Interaction Designer</option>
+												<option value="visual">Visual Interface Designer</option>
+												<option value="marketing">Online Marketeer</option>
+											</select>
+										</div>
+									</div>
+
+									<div class="middle">
+										<a href="" class="btn" alt="submit invite">Send invite</a>
+									</div>
+								</div>
+							</section>
+						</div>
+						<div class="row">
+							<section class="team-member-invite middle">
+								<div class="vertical-align">
+									<div class="middle">
+										<div class="img-circle" style='background-image: url("[[../../../../../../img/profile-picture-small.jpg]]")'></div>
+									</div>
+
+									<div class="middle member-name">
+										<h2 class="header-medium secondary">Francis Potter</h2>
+									</div>
+
+									<div class="middle">
+										<div class="select-dropdown">
+											<i class="icon icon-arrow-down"></i>
+											<select name="campaign-start-year" id="campaign-start-year" onchange="" size="1">
+												<option value="" disabled="" selected="">Member role</option>
+												<option value="front">Front-end Developer</option>
+												<option value="back">Back-end Developer</option>
+												<option value="manager">Project Manager</option>
+												<option value="content">Content Manager</option>
+												<option value="interaction">Interaction Designer</option>
+												<option value="visual">Visual Interface Designer</option>
+												<option value="marketing">Online Marketeer</option>
+											</select>
+										</div>
+									</div>
+
+									<div class="middle">
+										<a href="" class="btn" alt="submit invite">Send invite</a>
+									</div>
+								</div>
+							</section>
+						</div>
+					</section>			
 				</div>
 			</div>
 		</div>
